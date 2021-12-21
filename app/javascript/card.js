7d9a311ca96d98ba42eef8b09e7b3aefb12000d0
kosuke0720 <mayugevohn@gmail.com>
2021-12-21 22:33:34 +0900

    Update card.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -7,16 +7,17 @@ const pay = ()=> {
     const formResult = document.getElementById('charge-form');
     const formData = new FormData(formResult);
     const card = {
-      number: formData.get('order_address[number]'),
-      cvc: formData.get('order_address[cvc]'),
-      exp_month: formData.get('order_address[exp_month]'),
-      exp_year: `20${formData.get('order_address[exp_year]')}`,
+      number: formData.get('number'),
+      cvc: formData.get('cvc'),
+      exp_month: formData.get('exp-month'),
+      exp_year: `20${formData.get('exp-year')}`,
     };
+
+    console.log(card)
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
         const renderDom = document.getElementById('charge-form');
         const tokenObj = `<input value=${token} type='hidden' name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
