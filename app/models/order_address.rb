ef2bbbad0b00e619e242ffd98280da08200cb963
kosuke0720 <mayugevohn@gmail.com>
2021-12-23 17:29:19 +0900

    Update order_address.rb

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -13,7 +13,7 @@ class OrderAddress
     validates :address
     validates :phone_number,format: {with: /\A\d{10,11}\z/, message: "is invalid."}
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
         order = Order.create(user_id: user_id, item_id: item_id)
