a93fe65e6fde0c267ec1a07cf45bf8150e474941
kosuke0720 <mayugevohn@gmail.com>
2021-12-25 14:03:20 +0900

    Update order_address.rb

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -16,8 +16,7 @@ class OrderAddress
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-        order = Order.create(user_id: user_id, item_id: item_id)
-        binding.pry
+        order = Order.create!(user_id: user_id, item_id: item_id)
         Address.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number,order_id: order.id)
   end
 
