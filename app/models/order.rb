7acfe176faee8078a3a13fb8c27f925c0df4fe07
kosuke0720 <mayugevohn@gmail.com>
2021-12-25 14:03:18 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,7 @@
 class Order < ApplicationRecord
   attr_accessor :token
   belongs_to :user  
-  belongs_to :items  
+  belongs_to :items , optional: true
   has_one :address 
-
-  validates :token, presence: true
 
 end
