929ee56c976a13d388d30b97b21962c250a32aea
kosuke0720 <mayugevohn@gmail.com>
2022-01-03 14:47:53 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root to: 'users#new'
   resources :users, only: %i[show new edit update] 
+  resources :favorites, only: %i[create destroy]
   
   resources :items do
     resources :orders, only: %i[index create]
