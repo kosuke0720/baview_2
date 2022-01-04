0395cbf4691db52138168f00e113d66f59bd7dff
kosuke0720 <mayugevohn@gmail.com>
2022-01-04 16:38:10 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,17 @@
 class FavoritesController < ApplicationController
 
   def new
+    @favorite = Favorite.new
   end
 
   def create
+    @favorite = Favorite.new
+    @article = Article.find(:id)
+    @baby = Baby.find(:baby_id)
+    if @favorite.save
+      redirect_to baby_article_path(@baby.id, @article.id)
+    else
+      render baby_article_path(@baby.id, @article.id)
   end
 
   def destroy
