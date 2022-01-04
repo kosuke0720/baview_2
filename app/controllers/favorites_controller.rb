50c997c8ef8b070d86778722dd9ee06addcb609d
kosuke0720 <mayugevohn@gmail.com>
2022-01-04 16:45:10 +0900

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.Favorite.new(post_id: clicked_post.id)
-    if @favorite.save
+    if favorite.save
       redirect_back(fallback_location: root_path)
     else
       render baby_article_path(@baby.id, @article.id)
