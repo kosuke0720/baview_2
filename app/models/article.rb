d18e8b4182c50ac3a5093e6d0ab256018b1546a0
kosuke0720 <mayugevohn@gmail.com>
2022-01-05 20:57:03 +0900

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :baby, optional: true
   has_many :favorites, dependent: :destroy
-  has_many :users, trough: :favorite
+  has_many :users, through: :favorite
   has_one_attached :image
 
   validates :image, presence: true
